01fc082ea1f2d3566c629f14ce1565233827c42b
Larry Tambascio <larry@tambascio.net>
2012-02-03 00:54:43 -0500

    Functionally working, but needs a little clean up before sending in.

--- src/main/java/cscie160/hw1/Elevator.java
+++ src/main/java/cscie160/hw1/Elevator.java
@@ -8,21 +8,28 @@ package cscie160.hw1;
  */
 public class Elevator
 {
-	private final static int CAPACITY = 10;
+	public final static int CAPACITY = 10;
 	
-	private final static int FLOORS = 7;
+	public final static int FLOORS = 7;
 	
-	private final static boolean UP = true;
+	public final static boolean UP = true;
 	
-	private final static boolean DOWN = false;
+	public final static boolean DOWN = false;
 	
+	/**
+	 * This is the 0 based index array.  The display floor will be + 1 to this.
+	 */
 	private int currentFloor;
 	
 	private int passengerCnt;
 	
 	private boolean currentDirection;
 	
-	private boolean[] destination;
+	/**
+	 * Number of passengers destined for a particular floor.  Zero implies
+	 * nobody is getting off at that floor.
+	 */
+	private int[] destination;
 	
 	/**
 	 * No arg constructor to initiate the elevator's state.  The elevator is
@@ -31,13 +38,35 @@ public class Elevator
 	 */
 	public Elevator()
 	{
-		currentFloor = 1;
+		/**
+		 * Floors in buildings are 1 based, while arrays are 0 based.  Will 
+		 * need to adjust.
+		 */
+		this.currentFloor = 0;
 		
-		passengerCnt = 0;
+		this.passengerCnt = 0;
 		
-		currentDirection = UP;
+		this.currentDirection = UP;
 		
-		destination = new boolean[FLOORS];
+		this.destination = new int[FLOORS];
+	}
+	
+	/**
+	 * Main method to demonstrate how the elevator behaves.
+	 * 
+	 * @param argv	Array of string command line parameters
+	 */
+	public static void main (String args[])
+	{
+		Elevator elevator = new Elevator();
+		
+		elevator.boardPassenger(2);
+		elevator.boardPassenger(2);
+		elevator.boardPassenger(3);
+		
+		System.out.println(elevator);
+		
+		elevator.move();
 	}
 	
 	/**
@@ -47,16 +76,62 @@ public class Elevator
 	 */
 	public void move ()
 	{
-		if (currentDirection)	// true => up
+		do
 		{
-			if (currentFloor < FLOORS)
-				currentFloor++;
+			if (currentDirection)	// true => up
+			{
+				while (currentFloor < FLOORS - 1 && passengerCnt > 0)
+				{
+					currentFloor++;
+					if (destination[currentFloor] > 0)
+					{
+						stop();
+						break;
+					}
+				}
+				if (currentFloor == FLOORS - 1)
+					currentDirection = DOWN;
+				
+			}
+			else	// going down
+			{
+				while (currentFloor > 0 && passengerCnt > 0)
+				{
+					currentFloor--;
+					if (destination[currentFloor] > 0)
+					{
+						stop();
+						break;
+					}
+				}
+				if (currentFloor == 0)
+					currentDirection = UP;
+			}
 		}
-		else
-		{
-			if (currentFloor > 1)
-				currentFloor--;
-		}
+		while (passengerCnt > 0);
+	}
+	
+	/**
+	 * Method to discharge passengers and update state.
+	 */
+	public void stop()
+	{
+		passengerCnt -= destination[currentFloor];
+		destination[currentFloor] = 0;
+		System.out.println(this);
+	}
+	
+	/**
+	 * Adds one passenger to the elevator, with a specific floor as the
+	 * destination
+	 * 
+	 * @param floor		Destination floor for the passenger boarding the 
+	 * 					elevator
+	 */
+	public void boardPassenger(int floor)
+	{
+		this.destination[floor - 1]++;
+		this.passengerCnt++;
 	}
 
 	/**
@@ -65,8 +140,70 @@ public class Elevator
 	@Override
 	public String toString()
 	{
-		return "Elevator [currentFloor=" + currentFloor + ", currentDirection="
-				+ currentDirection + ", passengerCnt=" + passengerCnt + "]";
+		return "Elevator [currentFloor=" + getCurrentFloor() + ", currentDirection="
+				+ (currentDirection ? "Up" : "Down") + ", passengerCnt=" + passengerCnt + "]";
+	}
+	
+	// Getters and setters to facilitate testing
+
+	/**
+	 * Gets the current floor number.
+	 * @return	Adjust current floor to show the floor number as opposed to the 
+	 * 			array index.
+	 */
+	int getCurrentFloor()
+	{
+		return currentFloor + 1;
+	}
+
+	/**
+	 * Sets the current floor number, and adjusts it to represent the array
+	 * index.
+	 * @param currentFloor
+	 */
+	void setCurrentFloor(int currentFloor)
+	{
+		this.currentFloor = currentFloor - 1;
+	}
+
+	int getPassengerCnt()
+	{
+		return passengerCnt;
+	}
+
+	void setPassengerCnt(int passengerCnt)
+	{
+		this.passengerCnt = passengerCnt;
+	}
+
+	boolean isDirectionUp()
+	{
+		return currentDirection;
+	}
+
+	void setCurrentDirection(boolean currentDirection)
+	{
+		this.currentDirection = currentDirection;
+	}
+
+	int[] getDestination()
+	{
+		return destination;
+	}
+	
+	int getDestination(int floor)
+	{
+		return destination[floor - 1];
+	}
+
+	void setDestination(int[] destination)
+	{
+		this.destination = destination;
+	}
+	
+	void setDestination(int floor, int passengers)
+	{
+		this.destination[floor - 1] = passengers;
 	}
 
 }
--- src/test/java/cscie160/hw1/TestElevator.java
+++ src/test/java/cscie160/hw1/TestElevator.java
@@ -1,14 +1,99 @@
 package cscie160.hw1;
 
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-import org.testng.Assert;
+import static org.testng.Assert.assertEquals;
 
+/**
+ * Class to test the Elevator object.
+ * 
+ * @author Larry Tambascio
+ */
 public class TestElevator
 {
-	@Test
-	public void f()
+	private Elevator elevator;
+	
+	@BeforeMethod
+	public void setUp()
 	{
-		//Assert.fail("unimplemented test");
+		elevator = new Elevator();
+	}
+	
+	/**
+	 * Test the moving up from the first floor
+	 */
+	@Test
+	public void testFirstMove()
+	{
+		elevator.boardPassenger(2);
+		elevator.move();
+		assertEquals(elevator.getCurrentFloor(), 2, "test first move");
+	}
+	
+	/**
+	 * Test moving down at the bottom floor
+	 */
+	@Test
+	public void testMoveDownAtBottom()
+	{
+		elevator.setCurrentDirection(false);
+		elevator.move();
+		assertEquals(elevator.getCurrentFloor(), 1, "test move down at bottom");
+		assertEquals(elevator.isDirectionUp(), true, "direction didn't change " +
+				"at the bottom");
+	}
+	
+	/**
+	 * Basic moving down test
+	 */
+	@Test
+	public void testMoveDown()
+	{
+		elevator.setCurrentFloor(3);
+		elevator.boardPassenger(2);
+		elevator.setCurrentDirection(false);
+		elevator.move();
+		assertEquals(elevator.getCurrentFloor(), 2, "Test move down");
+	}
+	
+	/**
+	 * Test moving up at the top
+	 */
+	@Test
+	public void testMoveUpAtTop()
+	{
+		elevator.boardPassenger(7);
+		elevator.move();
+		assertEquals(elevator.getCurrentFloor(), 7, "test move up at top");
+		assertEquals(elevator.isDirectionUp(), false, "direction didn't change " +
+				"at the top");
+	}
+	
+	/**
+	 * Test boarding a passenger for the fifth floor.
+	 */
+	@Test
+	public void testBoardPassenger()
+	{
+		elevator.boardPassenger(5);
+		assertEquals(elevator.getPassengerCnt(), 1, "wrong number of " +
+				"passengers");
+		int[] dest = elevator.getDestination();
+		assertEquals(dest[4], 1, "wrong number of passengers for floor");
+	}
+	
+	/**
+	 * Test the operations of the stop method.
+	 */
+	@Test
+	public void testStop()
+	{
+		elevator.setDestination(3, 3);
+		elevator.setPassengerCnt(3);
+		elevator.setCurrentFloor(3);
+		elevator.stop();
+		assertEquals(elevator.getPassengerCnt(), 0, "left some passengers");
+		assertEquals(elevator.getDestination(3), 0, "still some passengers on 3");
 	}
 
 }
